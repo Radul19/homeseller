125b0e8f018eb79b88ea3739e6c1c17d8d9c802d
Radul19 <radulito19@gmail.com>
2021-11-21 18:47:24 -0400

    comments

--- src/api/useHandleErr.jsx
+++ src/api/useHandleErr.jsx
@@ -1,8 +1,16 @@
 import { useContext } from 'react'
 import { UserContext } from './userContext'
-export const useHandleErr = () => {
 
+/*
+Esto es un hook, no es un componente , si no una funcion, pero tiene las propiedades de un componente
+Si intentas usar propiedades de un hook (useState,useRef...) en una funcion normal, no te va a dejar
+Para eso usamos Hooks
+*/
+export const useHandleErr = () => {
+    /// Extraemos de UserContext
     const { setFade, setLoad, setMsg } = useContext(UserContext)
+    /// Y esta es la funcion que nos interesa, handleError
+    /// Cuando recibe un error, termina la animacion de la pantalla de carga y muestra el mensaje
     const handleError = (res)=>{
         setFade("out")
         setMsg({
@@ -14,7 +22,9 @@ export const useHandleErr = () => {
             setFade("in")
         }, 500);
     }
-
+    
+    /// Este return pertenece al Hook, de esta forma podemos acceder a la funcion externamente
+    /// const {handleError} = useHandleErr()
     return({
         handleError
     })
